3e9852a07295ce80c9181b1e70038733447711f3
Michał Pielacha <mr.cheesewick@gmail.com>
2021-04-10 10:13:47 +0200

    Task functionality added (buggy)

---
+++ src/main/java/com/example/config/WebSecurityConfig.java
@@ -0,0 +1,52 @@
+package com.example.config;
+
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("test")
+                .password("{noop}test")
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password("{noop}admin")
+                .roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "/index", "/personList")
+                .hasAuthority("ROLE_USER")
+                .antMatchers("/technology", "/tasks")
+                .hasAuthority("ROLE_ADMIN")
+                .and()
+                .csrf()
+                .disable()
+                .headers()
+                .frameOptions()
+                .disable()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .loginProcessingUrl("/login")
+                .failureForwardUrl("/login?error")
+                .defaultSuccessUrl("/index")
+                .and()
+                .logout()
+                .logoutSuccessUrl("/login");
+    }
+
+}
